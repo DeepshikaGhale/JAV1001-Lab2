1264f7e436ad97a8a44e40d198b911b37f12f12f
Deepshika Ghale <58839663+DeepshikaGhale@users.noreply.github.com>
2023-02-09 19:33:30 -0500

    Create arrayContains method

--- ArrayTools.kt
+++ ArrayTools.kt
@@ -10,9 +10,29 @@ fun main(){
     val array = arrayOf<Int>(1,2,3,4,5)
     val reversedArray = reverse(array)
     println(reversedArray.contentToString())
+
+    val contains = arrayContains(array, 6)
+    println(contains)
 }
 
-//reverse function to reverse array
+//accepts an array and a search value, and determines if the array contains the value (true/false)
+fun arrayContains(array: Array<Int>, searchValue: Int): Boolean{
+
+    //loop goes through the element of given parameter array
+    for (i in array.indices){
+
+        //if search value is equal to any element of given array then it returns true
+        if(searchValue == array[i]){
+            return true
+        }
+    }
+
+    //if the search value is not found in the array
+    return false
+}
+
+
+//reverses an array
 fun reverse(originalArray: Array<Int>): Array<Int>{
     //store reversed array
     var reversedArray = arrayOf<Int>()
