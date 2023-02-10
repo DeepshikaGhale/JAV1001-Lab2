ba5fa7d8c3e976f056b4b037fc298497fa67db20
Deepshika Ghale <58839663+DeepshikaGhale@users.noreply.github.com>
2023-02-09 19:27:15 -0500

    Create reverse array function

--- ArrayTools.kt
+++ ArrayTools.kt
@@ -7,6 +7,21 @@
 */
 
 fun main(){
+    val array = arrayOf<Int>(1,2,3,4,5)
+    val reversedArray = reverse(array)
+    println(reversedArray.contentToString())
+}
 
+//reverse function to reverse array
+fun reverse(originalArray: Array<Int>): Array<Int>{
+    //store reversed array
+    var reversedArray = arrayOf<Int>()
+
+    //create loop to pass index according to the size of the original array
+    for(i in originalArray.size - 1 downTo 0){
+        reversedArray += originalArray[i]
+    }
+
+    return  reversedArray
 }
 
